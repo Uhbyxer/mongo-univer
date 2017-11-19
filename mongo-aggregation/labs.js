abd3f1874d08dfb4d0caf458eddf5ad3db8d52ba
Taras Hrynchuk <Taras_Hrynchuk@epam.com>
2017-11-19 17:58:03 +0200

    lab 2, 3

--- mongo-aggregation/labs.js
+++ mongo-aggregation/labs.js
@@ -9,3 +9,26 @@ var pipeline = [{ $match: { "imdb.rating" : {$gte: 7},
 ];
 
 db.getCollection('movies').aggregate(pipeline).itcount()
+
+
+//============= lab 2
+var pipeline = [{ $match: { "imdb.rating" : {$gte: 7},  
+    "genres" : {$nin: ["Crime" ,"Horror"]},
+    "rated" : {$in: ["PG" ,"G"]} ,
+    "languages" : {$all: ["English", "Japanese"]}  
+}},
+    {$project: 
+        {_id: 0, title: 1, rated: 1}
+    }
+];
+
+db.getCollection('movies').aggregate(pipeline).itcount()
+
+//============= lab 3
+var pipeline = [
+    {$project: {title: 1,  wordsCount: {$size: {$split: ["$title", " "]}}},        
+    },
+    {$match: {"wordsCount": {$eq: 1}}}
+];
+
+db.getCollection('movies').aggregate(pipeline).itcount()
