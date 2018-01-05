bbecc62274b155dc22905b2e26aa4fdd5921b905
Taras Hrynchuk <Taras_Hrynchuk@epam.com>
2018-01-05 17:32:41 +0200

    expr, arrayFilters

--- mongo-features-36/labs.js
+++ mongo-features-36/labs.js
@@ -1,10 +1,17 @@
 
 
+
+////////////    $expr
 db.movies.find(
     {       
        $expr: { $gt : [ { $divide: [ "$metacritic", 10 ] }, "$imdb.rating" ] }
     }
  ).count();
 
+////////////    arrayFilters
 
- 
+db.airline_claims.updateMany(
+    {},
+    { $unset: {"airlines.$[airlines]" : ""} },
+    {arrayFilters: [{"airlines.airlineName":"-"}]}
+)
