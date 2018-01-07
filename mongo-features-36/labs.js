1fbb425eb225b5af3b0a6a015072030dc8ca869d
Taras Hrynchuk <Taras_Hrynchuk@epam.com>
2018-01-07 17:50:57 +0200

    lookup lab

--- mongo-features-36/labs.js
+++ mongo-features-36/labs.js
@@ -15,3 +15,53 @@ db.airline_claims.updateMany(
     { $unset: {"airlines.$[airlines]" : ""} },
     {arrayFilters: [{"airlines.airlineName":"-"}]}
 )
+
+//lookup
+
+db.air_alliances.aggregate([
+    {
+      "$lookup": {
+        "from": "air_routes",
+        "let": {
+          "airlines": "$airlines"
+        },
+        "pipeline": [{
+               $match:
+                   { $expr:
+                          { $in: [ "$airline.name", "$$airlines"] }
+                         
+                   }
+                
+          },
+          {
+            "$project": {
+              "_id": 0,
+              "airplanes": "$airplane"
+            }
+          }
+        ],
+        "as": "fleet"
+      }
+    },
+    {
+      "$project": {
+        "name": 1,
+        "fleet": {
+          "$size": {
+            "$reduce": {
+              "input": "$fleet.airplanes",
+              "initialValue": [],
+              "in": {
+                "$setUnion": [
+                  "$$value",
+                  {
+                    "$split": ["$$this", " "]
+                  }
+                ]
+              }
+            }
+          }
+        }
+      }
+    }
+  ])
